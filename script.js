1fb42a179a848e9ad6b5dc4aeb60255da6f515ce
Tolibjon04 <tolibsaydulloyevich1@gmail.com>
2024-04-27 14:15:30 +0500

    fixing

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var otherLinks = document.getElementsByClassName("othera");
-  for (var i = 0; i < otherLinks.length; i++) {
-    otherLinks[i].addEventListener("click", function (event) {
-      event.preventDefault();
+// document.addEventListener("DOMContentLoaded", function () {
+//   var otherLinks = document.getElementsByClassName("othera");
+//   for (var i = 0; i < otherLinks.length; i++) {
+//     otherLinks[i].addEventListener("click", function (event) {
+//       event.preventDefault();
 
-      var loading = document.getElementById("loading");
-      loading.style.display = "block";
+//       var loading = document.getElementById("loading");
+//       loading.style.display = "block";
 
-      setTimeout(function () {
-        window.location.href = event.target.href;
-      }, 500);
-    });
-  }
-  var thisLinks = document.getElementsByClassName("thisa");
-  for (var j = 0; j < thisLinks.length; j++) {
-    thisLinks[j].addEventListener("click", function (event) {
-      event.preventDefault();
+//       setTimeout(function () {
+//         window.location.href = event.target.href;
+//       }, 500);
+//     });
+//   }
+//   var thisLinks = document.getElementsByClassName("thisa");
+//   for (var j = 0; j < thisLinks.length; j++) {
+//     thisLinks[j].addEventListener("click", function (event) {
+//       event.preventDefault();
 
-      var loading = document.getElementById("loading");
-      loading.style.display = "block";
+//       var loading = document.getElementById("loading");
+//       loading.style.display = "block";
 
-      setTimeout(function () {
-        window.location.href = event.target.href;
-      }, 500);
-    });
-  }
-});
+//       setTimeout(function () {
+//         window.location.href = event.target.href;
+//       }, 500);
+//     });
+//   }
+// });
 
 window.addEventListener("scroll", function () {
   var scrollPosition = window.scrollY;
